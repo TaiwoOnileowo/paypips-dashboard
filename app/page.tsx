f276476ded032b4063ad1435ca30da859ed84846
Taiwo Onileowo <taiwoonileowo17@gmail.com>
2024-09-27 23:44:02 +0100

    add async to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { redirect } from "next/navigation";
-const Page = () => {
+const Page = async () => {
   redirect("/dashboard");
   return <div>Paypips Dashboard</div>;
 };
