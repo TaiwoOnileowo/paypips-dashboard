6426a60f86c2cbddf5ea40456e44c9c193aedcae
Taiwo Onileowo <taiwoonileowo17@gmail.com>
2024-09-30 17:50:00 +0100

    increase react query global stale and garbage time

--- lib/http.ts
+++ lib/http.ts
@@ -5,8 +5,8 @@ import { QueryClient } from "@tanstack/react-query";
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 2 * 60 * 1000,
-      gcTime: 3 * 60 * 1000,
+      staleTime: 3 * 60 * 1000,
+      gcTime: 5 * 60 * 1000,
     },
   },
 });
