35bdfdbad32753304eb00e2905c3d3156d2afee1
Taiwo Onileowo <taiwoonileowo17@gmail.com>
2024-10-01 23:28:42 +0100

    remove next Image

--- components/(dashboard)/Error.tsx
+++ components/(dashboard)/Error.tsx
@@ -4,7 +4,7 @@ import errorGif from "@/assets/icons/error.gif";
 const Error = ({ message }: { message: string }) => {
   return (
     <div className="flex items-center h-full flex-col pt-6 ">
-      <Image src={errorGif} alt="error" width={100} height={100} />
+      <img src={errorGif.src} alt="error" width={100} height={100} />
       <p className="text-center text-gray-300/80 mt-2">{message}</p>
     </div>
   );
--- components/(dashboard)/NotFound.tsx
+++ components/(dashboard)/NotFound.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import dollar from "@/assets/icons/dollar2.gif";
+import dollar from "@/assets/icons/dollar1.gif";
 import Image from "next/image";
 const NotFound = ({ message }: { message: string }) => {
   return (
     <div className="flex items-center pt-10 h-full flex-col w-full">
-      <Image src={dollar} alt="dollar" width={100} height={100} />
+      <img src={dollar.src} alt="dollar" width={100} height={100} />
       {message}
     </div>
   );
