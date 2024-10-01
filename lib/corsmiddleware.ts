a68387745ae326e07fa9ec9d2ce7d8f21f4aa53f
Taiwo Onileowo <taiwoonileowo17@gmail.com>
2024-10-01 17:09:45 +0100

    add cors middleware

--- lib/corsmiddleware.ts
+++ lib/corsmiddleware.ts
@@ -1,33 +1,79 @@
-// @ts-nocheck
-import { NextResponse } from "next/server";
-export function corsMiddleware(handler) {
-  return async (req) => {
+import Cors from "cors";
+
+const cors = Cors({
+  origin: ["https://paypips-dash.vercel.app", "https://www.paypipsbot.com"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "X-Extension-ID",
+    "Access-Control-Allow-Origin",
+  ],
+  credentials: true,
+});
+
+function runMiddleware(
+  req: { method: string; headers: { origin: any } },
+  res: {
+    setHeader: (arg0: string, arg1: string) => void;
+    status: (arg0: number) => {
+      (): any;
+      new (): any;
+      end: { (): void; new (): any };
+    };
+  },
+  fn: (arg0: any, arg1: any, arg2: (result: any) => void) => void
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
+
+export default function corsMiddleware(handler: (arg0: any, arg1: any) => any) {
+  return async (
+    req: { method: string; headers: { origin: any } },
+    res: {
+      setHeader: (arg0: string, arg1: string) => void;
+      status: (arg0: number) => {
+        (): any;
+        new (): any;
+        end: { (): void; new (): any };
+      };
+    }
+  ) => {
     if (req.method === "OPTIONS") {
       const allowedOrigins = [
+        "https://paypips-dash.vercel.app",
         "https://www.paypipsbot.com",
-        "https://paypips-dashboard.vercel.app/",
       ];
 
-      const origin = req.headers.get("origin");
+      const origin = req.headers.origin;
 
       if (allowedOrigins.includes(origin)) {
-        return NextResponse.next({
-          headers: {
-            "Access-Control-Allow-Origin": origin,
-            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-            "Access-Control-Allow-Headers":
-              "Content-Type, Authorization, X-Extension-ID",
-            "Access-Control-Allow-Credentials": "true",
-          },
-        });
+        res.setHeader("Access-Control-Allow-Origin", origin);
+      } else {
+        res.setHeader("Access-Control-Allow-Origin", "*"); // For testing purposes
       }
 
-      return NextResponse.json(
-        { message: "CORS blocked the request." },
-        { status: 403 }
+      res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS"
       );
+      res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization, X-Extension-ID"
+      );
+      res.setHeader("Access-Control-Allow-Credentials", "true");
+      res.status(204).end();
+      return;
     }
 
-    return handler(req);
+    await runMiddleware(req, res, cors);
+    return handler(req, res);
   };
 }
