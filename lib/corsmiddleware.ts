d749ef3ba73ba84cb9e4e7df760f035df5e2f0ed
Taiwo Onileowo <taiwoonileowo17@gmail.com>
2024-10-01 17:49:07 +0100

    cors fix

--- lib/corsmiddleware.ts
+++ lib/corsmiddleware.ts
@@ -5,38 +5,52 @@ export default function corsMiddleware(
 ) {
   return async (req: NextRequest) => {
     // Handle OPTIONS request for CORS
-    if (req.method === "OPTIONS") {
-      const allowedOrigins = [
-        "https://paypips-dash.vercel.app",
-        "https://www.paypipsbot.com",
-      ];
+    // if (req.method === "OPTIONS") {
+    //   const allowedOrigins = [
+    //     "https://paypips-dash.vercel.app",
+    //     "https://www.paypipsbot.com",
+    //     "http://localhost:3000/",
+    //   ];
 
-      const origin = req.headers.get("origin");
+    //   const origin = req.headers.get("origin");
 
-      const response = new NextResponse(null, { status: 204 });
+    //   const response = new NextResponse(null, { status: 204 });
 
-      if (allowedOrigins.includes(origin || "")) {
-        response.headers.set("Access-Control-Allow-Origin", origin!);
-      } else {
-        response.headers.set("Access-Control-Allow-Origin", "*"); // For testing
-      }
+    //   if (allowedOrigins.includes(origin || "")) {
+    //     response.headers.set("Access-Control-Allow-Origin", origin!);
+    //   } else {
+    //     response.headers.set("Access-Control-Allow-Origin", "*"); // For testing
+    //   }
 
-      response.headers.set(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, OPTIONS"
-      );
-      response.headers.set(
-        "Access-Control-Allow-Headers",
-        "Content-Type, Authorization, X-Extension-ID"
-      );
-      response.headers.set("Access-Control-Allow-Credentials", "true");
-
-      return response;
-    }
+    //   response.headers.set(
+    //     "Access-Control-Allow-Methods",
+    //     "GET, POST, PUT, DELETE, OPTIONS"
+    //   );
+    //   response.headers.set(
+    //     "Access-Control-Allow-Headers",
+    //     "Content-Type, Authorization,"
+    //   );
+    //   response.headers.set("Access-Control-Allow-Credentials", "true");
+    //   console.log(response, origin);
+    //   return response;
+    // }
 
     // Continue with the request if not OPTIONS
-    const res = await handler(req, NextResponse.next());
-    res.headers.set("Access-Control-Allow-Origin", "*");
-    return res;
+    const response = await handler(req, NextResponse.next());
+
+    const origin =req.headers.get("origin") || req.headers.get("referer") || "*"
+    response.headers.set("Access-Control-Allow-Origin", origin!);
+    response.headers.set(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    response.headers.set(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization,"
+    );
+    response.headers.set("Access-Control-Allow-Credentials", "true");
+    response.headers.set("Access-Control-Allow-Origin", "*");
+    console.log(response, origin);
+    return response;
   };
 }
